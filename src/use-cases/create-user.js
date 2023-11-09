da6ff69fa4cebb7c067154de98f12b0be315f365
Caio Reges <caioreges@hotmail.com>
2023-11-08 23:18:43 -0300

    feat: add CreateUserUseCase

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,29 @@
+import { v4 as uuidv4 } from 'uuid'
+import bcrypt from 'bcrypt'
+import { PostgresCreateUserRepository } from '../repositories/postrgres/create-user'
+
+export class CreateUserUseCase {
+    async execute(createUserParams) {
+        // TODO: verificar se o email já está em uso
+
+        //gerar ID do usuário
+        const userID = uuidv4()
+
+        // criptografar a senha
+        const hashedPass = await bcrypt.hash(createUserParams.password, 10)
+
+        // inserir o usuário no banco de dados
+        const user = {
+            ...createUserParams,
+            id: userID,
+            password: hashedPass,
+        }
+
+        // chamar o repositório
+        const postgresCreateUserRepository = new PostgresCreateUserRepository()
+
+        const createdUser = await postgresCreateUserRepository.execute(user)
+
+        return createdUser
+    }
+}
