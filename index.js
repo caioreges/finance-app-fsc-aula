5731c9e683fbbcb8c65c1b961f61c608e78c532f
Caio Reges <caioreges@hotmail.com>
2023-11-08 23:01:18 -0300

    feat: add PostgresCreatUserRepository

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 import express from 'express'
 
-import { PostgresHelper } from './src/db/postgres/helper.js'
-
 const app = express()
 
-app.get('/', async (req, res) => {
-    const results = await PostgresHelper.query('SELECT * FROM users;')
+app.use(express.json())
 
-    res.send(JSON.stringify(results))
-})
-
-app.listen(process.env.PORT, () => console.log('listening on port 3000'))
+app.listen(process.env.PORT, () => console.log('listening on port 8000'))
---
+++ src/repositories/postrgres/create-user.js
@@ -0,0 +1,21 @@
+import { PostgresHelper } from '../../db/postgres/helper'
+
+export class PostgresCreateUserRepository {
+    async execute(createUserParams) {
+        // create user in postgres
+        const results = await PostgresHelper.query(
+            'INSERT INTO users (ID, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
+            [
+                createUserParams.ID,
+                createUserParams.first_name,
+                createUserParams.last_name,
+                createUserParams.email,
+                createUserParams.password,
+            ],
+        )
+
+        return results[0]
+    }
+}
+
+PostgresCreateUserRepository.execute()
