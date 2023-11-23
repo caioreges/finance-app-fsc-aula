c92a3a134b12bb42b593d988973557c49ca076cb
Caio Reges <caioreges@hotmail.com>
2023-11-22 22:33:30 -0300

    feat: added 'ok' message return and 'Internal Server Error' correction

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -1,20 +1,21 @@
-export const badRequest = (body) => {
-    return {
-        statusCode: 400,
-        body,
-    }
-}
+export const badRequest = (body) => ({
+    statusCode: 400,
+    body,
+})
 
-export const created = (body) => {
-    return {
-        statusCode: 201,
-        body,
-    }
-}
+export const created = (body) => ({
+    statusCode: 201,
+    body,
+})
 
-export const serverError = (body) => {
-    return {
-        statusCode: 500,
-        body,
-    }
-}
+export const serverError = () => ({
+    statusCode: 500,
+    body: {
+        message: 'Internal server error',
+    },
+})
+
+export const ok = (body) => ({
+    statusCode: 200,
+    body,
+})
